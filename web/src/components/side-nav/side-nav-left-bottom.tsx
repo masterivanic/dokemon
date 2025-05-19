718ed93e3e48e4c80dddd34c8780fa3203854c7e
Albert Einstein <javastraat@hotmail.com>
2025-05-19 12:51:36 +0200

    revoke page fix on mobile

--- web/src/components/side-nav/side-nav-left-bottom.tsx
+++ web/src/components/side-nav/side-nav-left-bottom.tsx
@@ -46,7 +46,7 @@ export function SideNavLeftBottom() {
             className="h-6 w-6 shrink-0 text-gray-400"
             aria-hidden="true"
           />
-          Github
+          Our Github
         </a>
       </li>
       <li>
@@ -58,7 +58,7 @@ export function SideNavLeftBottom() {
           )}
         >
           <LifebuoyIcon className="h-6 w-6 shrink-0" aria-hidden="true" />
-          Support
+          Discord Support
         </a>
       </li>
       <li>
