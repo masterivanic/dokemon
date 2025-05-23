d50043598f66a05740b1618b974b5a57727cd1f2
Albert Einstein <javastraat@hotmail.com>
2025-05-23 12:01:50 +0200

    make server arm/v7 version

--- make-server-windows.bat
+++ make-server-windows.bat
@@ -1,3 +1,3 @@
 git pull
-docker buildx build --platform linux/amd64,linux/arm64 -t javastraat/dokemon-server:latest -f Dockerfile.server --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t javastraat/dokemon-server:latest -f Dockerfile.server --push .
 pause
