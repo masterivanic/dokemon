23e50263f6ff58b221f96148b429c89cfc7deb73
Albert Einstein <javastraat@hotmail.com>
2025-06-10 19:11:48 +0200

    cleanup

--- make-agent-windows.bat
+++
@@ -1,3 +0,0 @@
-git pull
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t javastraat/dokemon-agent:latest -f Dockerfile.agent --push .
-pause
--- make-server-windows.bat
+++
@@ -1,3 +0,0 @@
-git pull
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t javastraat/dokemon-server:latest -f Dockerfile.server --push .
-pause
